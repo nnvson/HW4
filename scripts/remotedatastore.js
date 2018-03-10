5376e47fbf0cd9a2fe4a228a0522fd2765458ec7
nnvson <nnv.son@gmail.com>
2018-03-10 15:35:59 -0800

    Finished with deployd and mongodb

---
+++ scripts/remotedatastore.js
@@ -0,0 +1,131 @@
+(function(window) {
+  "use strict";
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function RemoteDataStore(url) {
+    if (!url) {
+      throw new Error("No remote URL supplied.");
+    }
+    this.serverUrl = url;
+  }
+
+  RemoteDataStore.prototype.add = function(key, val) {
+    /*$.post(this.serverUrl, val, function(serverResponse) {
+      console.log(serverResponse);
+    });*/
+
+    $.ajax(this.serverUrl, {
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({
+        "coffee": val.coffee,
+        "emailAddress": val.emailAddress,
+        "flavor": val.flavor,
+        "size": val.size,
+        "strength": val.strength,
+        "id": val.id
+      }),
+      success: function(serverResponse) {
+        console.log(serverResponse);
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
+    });
+  };
+
+  RemoteDataStore.prototype.getAll = function(cb) {
+    /*$.get(this.serverUrl, function(serverResponse) {
+      console.log(serverResponse);
+      cb(serverResponse);
+    });*/
+
+    $.ajax(this.serverUrl, {
+      type: "GET",
+      success: function(serverResponse) {
+        console.log(serverResponse);
+        cb(serverResponse);
+      },
+      error: function(xhr) {
+        alert(xhr.responseText);
+      }
+    });
+  };
+
+
+  RemoteDataStore.prototype.get = function(key, cb) {
+    /*$.get(this.serverUrl + "/" + key, function(serverResponse) {
+      console.log(serverResponse);
+      cb(serverResponse);
+    });*/
+
+    $.ajax({
+      method: "GET",
+      url: this.serverUrl,
+      success: function(serverResponse) {
+        console.log(serverResponse);
+
+        //get id
+        var id;
+        for (var i = 0; i < serverResponse.length; i++) {
+          if (serverResponse[i].emailAddress == key) {
+            id = serverResponse[i].id;
+          }
+        }
+        console.log(id);
+
+        $.ajax({
+          method: "GET",
+          url: this.url + "/" + id,
+          success: function(serverResponse) {
+            console.log(this.url);
+            console.log(serverResponse);
+            cb(serverResponse);
+          },
+          error: function(xhr) {
+            alert(xhr.responseText);
+          }
+        });
+      },
+    });
+  };
+
+  RemoteDataStore.prototype.remove = function(key) {
+    /*$.ajax(this.serverUrl + "/" + key, {
+      type: "DELETE"
+    });*/
+
+    $.ajax({
+      method: "GET",
+      url: this.serverUrl,
+      success: function(serverResponse) {
+        console.log(serverResponse);
+
+        //get id
+        var id;
+        for (var i = 0; i < serverResponse.length; i++) {
+          if (serverResponse[i].emailAddress == key) {
+            id = serverResponse[i].id;
+          }
+        }
+        console.log(id);
+        $.ajax({
+          method: "DELETE",
+          url: this.url + "/" + id,
+          success: function(serverResponse) {
+            console.log(this.url);
+            console.log(serverResponse);
+            console.log("delete item " + id);
+          },
+          error: function(xhr) {
+            alert(xhr.responseText);
+          }
+        });
+      },
+    });
+  };
+
+  App.RemoteDataStore = RemoteDataStore;
+  window.App = App;
+})(window);
